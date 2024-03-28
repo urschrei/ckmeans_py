b08e4abe9fae5c416ce01762fc830c4d4df8635e
Stephan Hügel <shugel@tcd.ie>
2024-03-28 20:14:54 +0000

    Use abi3 and Python 3.10+

--- tests/test_ckmeans.py
+++ tests/test_ckmeans.py
@@ -3,7 +3,8 @@ from ckmeans import ckmeans
 
 
 def test_ckmeans() -> None:
-    data = np.array([1.0, 2.0, 3.0, 4.0, 100.0, 101.0, 102.0, 103.0])
+    data = np.array([1.0, 2.0, 3.0, 4.0, 100.0, 101.0, 102.0, 103.0, 104.0])
     clusters = 2
     result = ckmeans(data, clusters)
-    assert result == [[1.0, 2.0, 3.0, 4.0], [100.0, 101.0, 102.0, 103.0]]
+    assert list(result[0]) == [1.0, 2.0, 3.0, 4.0]
+    assert list(result[1]) == [100.0, 101.0, 102.0, 103.0, 104.0]
